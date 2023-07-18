fff68261aab0d242ec361715dce4d502f88991ea
AnoPiS <omer.mkawy.om@gmail.com>
2023-07-19 02:57:16 +0300

    set is done

--- lib/layout/home_layout.dart
+++ lib/layout/home_layout.dart
@@ -20,11 +20,12 @@ class _HomeLayoutState extends State<HomeLayout> {
   bool isButtonSheetShown = false;
   IconData fabIcon = Icons.edit;
   int currentIndex = 0;
+  List<Map> tasks=[];
   // TextEditingController instances for each input field
   final TextEditingController _titleController = TextEditingController();
   final TextEditingController _dateController = TextEditingController();
   final TextEditingController _timeController = TextEditingController();
-  final TextEditingController _statusController = TextEditingController();
+  // final TextEditingController _statusController = TextEditingController();
 
   late Database database;
   List<Widget> screens = [
@@ -35,7 +36,7 @@ class _HomeLayoutState extends State<HomeLayout> {
   @override
   void initState() {
     super.initState();
-    CreateDatabase();
+    createDatabase();
   }
 
   @override
@@ -62,115 +63,137 @@ class _HomeLayoutState extends State<HomeLayout> {
         backgroundColor: Styles.gumColor,
         onPressed: () {
           if (isButtonSheetShown) {
-            if(formKey.currentState!.validate()){
-              InsertToDatabase(
-                title:_titleController.text ,time: _timeController.text,date:_dateController.text ,
-              ).then((value){
+            if (formKey.currentState!.validate()) {
+              insertToDatabase(
+                title: _titleController.text,
+                time: _timeController.text,
+                date: _dateController.text,
+              ).then((value) {
                 Navigator.pop(context);
                 isButtonSheetShown = false;
                 setState(() {
                   fabIcon = Icons.edit;
-              });
+                });
               });
             }
-
           } else {
-            ScaffoldKey.currentState?.showBottomSheet(
-              (context) => FractionallySizedBox(
-                alignment: Alignment.center,
-                heightFactor: 0.45,
-                child: Container(
-                  color: Styles.lightBlackColor,
-                  child: SingleChildScrollView(
+            ScaffoldKey.currentState
+                ?.showBottomSheet(
+                  (context) => FractionallySizedBox(
+                    alignment: Alignment.center,
+                    heightFactor: 0.45,
                     child: Container(
                       color: Styles.lightBlackColor,
-                      child: Form(
-                        key: formKey,
-                        child: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            SizedBox(height: 35.0, width: double.infinity),
-                            buildTextField(
-                              context: context,
-                              labelText: 'Title',
-                              controller: _titleController,
-                              prefix: Icons.title_rounded,
-                              validate: (String? value) {
-                                if (value == null || value.isEmpty) {
-                                  return 'Please enter a title';
-                                }
-                                return null; // Return null to indicate the input is valid
-                              },
-                              type: TextInputType.text,
-                            ),
-                            SizedBox(height: 35.0, width: double.infinity),
-
-                            Wrap(
-                              spacing: 16.0,
-                              runSpacing: 16.0,
+                      child: SingleChildScrollView(
+                        child: Container(
+                          color: Styles.lightBlackColor,
+                          child: Form(
+                            key: formKey,
+                            child: Column(
+                              mainAxisSize: MainAxisSize.min,
                               children: [
-
+                                SizedBox(height: 35.0, width: double.infinity),
                                 buildTextField(
-                                    widthRit:0.4,
-                                    context: context,
-                                    labelText: 'Date',
-                                    controller: _dateController,
-                                    prefix: Icons.date_range_rounded,
-                                    type: TextInputType.datetime,
-                                    onTap: () {
-                                      showDatePicker(context:context,
-                                        initialDate: DateTime.now(),
-                                        firstDate: DateTime.now(),
-                                        lastDate: DateTime(2025),)
-                                      .then((date){_dateController.text=DateFormat.yMMMd().format(date!);});
-                                    }),
-                                buildTextField(
-                                    widthRit:0.4,
-                                    context: context,
-                                    labelText: 'Time',
-                                    controller: _timeController,
-                                    prefix: Icons.access_time_rounded,
-                                    type: TextInputType.datetime,
-                                    onTap: () {
-                                      showTimePicker(context: context, initialTime: TimeOfDay.now())
-                                          .then(
-                                        (time) {
-                                          _timeController.text =
-                                              time!.format(context);
-                                        },
-                                      ).catchError(
-                                        (error) {
-                                          _timeController.text = "";
-                                        },
-                                      );
-                                    }),
-                                SizedBox(height: 0, width: MediaQuery.of(context).size.width * 0.4),
-                                SizedBox(height: 0, width: MediaQuery.of(context).size.width * 0.4),
+                                  context: context,
+                                  labelText: 'Title',
+                                  controller: _titleController,
+                                  prefix: Icons.title_rounded,
+                                  validate: (String? value) {
+                                    if (value == null || value.isEmpty) {
+                                      return 'Please enter a title';
+                                    }
+                                    return null; // Return null to indicate the input is valid
+                                  },
+                                  type: TextInputType.text,
+                                ),
+                                SizedBox(height: 35.0, width: double.infinity),
+                                Wrap(
+                                  spacing: 16.0,
+                                  runSpacing: 16.0,
+                                  children: [
+                                    buildTextField(
+                                        widthRit: 0.4,
+                                        context: context,
+                                        labelText: 'Date',
+                                        controller: _dateController,
+                                        prefix: Icons.date_range_rounded,
+                                        type: TextInputType.datetime,
+                                        onTap: () {
+                                          showDatePicker(
+                                            context: context,
+                                            initialDate: DateTime.now(),
+                                            firstDate: DateTime.now(),
+                                            lastDate: DateTime(2025),
+                                          ).then((date) {
+                                            _dateController.text =
+                                                DateFormat.yMMMd()
+                                                    .format(date!);
+                                          });
+                                        }),
+                                    buildTextField(
+                                        widthRit: 0.4,
+                                        context: context,
+                                        labelText: 'Time',
+                                        controller: _timeController,
+                                        prefix: Icons.access_time_rounded,
+                                        type: TextInputType.datetime,
+                                        onTap: () {
+                                          showTimePicker(
+                                                  context: context,
+                                                  initialTime: TimeOfDay.now())
+                                              .then(
+                                            (time) {
+                                              _timeController.text =
+                                                  time!.format(context);
+                                            },
+                                          ).catchError(
+                                            (error) {
+                                              _timeController.text = "";
+                                            },
+                                          );
+                                        }),
+                                    SizedBox(
+                                        height: 0,
+                                        width:
+                                            MediaQuery.of(context).size.width *
+                                                0.4),
+                                    SizedBox(
+                                        height: 0,
+                                        width:
+                                            MediaQuery.of(context).size.width *
+                                                0.4),
 
-                                // buildTextField(
-                                //     context: context,
-                                //     labelText: 'Status',
-                                //     controller: _statusController,
-                                //     prefix: Icons.task_outlined,
-                                //     onTap: null,
-                                //     validate: (value) {
-                                //       if (value == null || value.isEmpty) {
-                                //         value = "new";
-                                //       }
-                                //       return null; // Return null to indicate the input is valid
-                                //     },
-                                //     type: TextInputType.text),
+                                    // buildTextField(
+                                    //     context: context,
+                                    //     labelText: 'Status',
+                                    //     controller: _statusController,
+                                    //     prefix: Icons.task_outlined,
+                                    //     onTap: null,
+                                    //     validate: (value) {
+                                    //       if (value == null || value.isEmpty) {
+                                    //         value = "new";
+                                    //       }
+                                    //       return null; // Return null to indicate the input is valid
+                                    //     },
+                                    //     type: TextInputType.text),
+                                  ],
+                                ),
+                                SizedBox(height: 60.0, width: double.infinity),
                               ],
                             ),
-                            SizedBox(height: 60.0, width: double.infinity),
-                          ],
+                          ),
                         ),
                       ),
                     ),
                   ),
-                ),
-              ),
-            );
+                )
+                .closed
+                .then((value) {
+              isButtonSheetShown = false;
+              setState(() {
+                fabIcon = Icons.edit;
+              });
+            });
             isButtonSheetShown = true;
             setState(() {
               fabIcon = Icons.add;
@@ -205,7 +228,7 @@ class _HomeLayoutState extends State<HomeLayout> {
     );
   }
 
-  void CreateDatabase() async {
+  void createDatabase() async {
     database = await openDatabase('todo.db', version: 1,
         onCreate: (database, version) {
       print('database created');
@@ -218,19 +241,29 @@ class _HomeLayoutState extends State<HomeLayout> {
         print('error creating table ${error.toStringg()}');
       });
     }, onOpen: (database) {
+      getDataFromDatabase(database).then((value)
+      {
+        tasks =value;
+        print(tasks);
+      });
       print('database opened');
     });
   }
 
-  Future InsertToDatabase({
-  required String title,
-  required String time,
-  required String date,
+  Future<List<Map>> getDataFromDatabase(database) async
+  {
+    return  await database.rawQuery('SELECT * FROM tasks');
+  }
 
-}) async {
+  Future insertToDatabase({
+    required String title,
+    required String time,
+    required String date,
+  }) async {
     await database.transaction((txn) async {
       // Example of inserting data into the 'tasks' table.
-      await txn.rawInsert('INSERT INTO tasks VALUES("$currentIndex","$title","$date","$time","new")');
+      await txn.rawInsert(
+          'INSERT INTO tasks VALUES("$currentIndex","$title","$date","$time","new")');
     }).then((_) {
       // The transaction was successful.
       print('$_ inserted successfully.');
